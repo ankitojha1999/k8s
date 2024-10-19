41df1398bcc3fc8e45c43821b23a4af2c507cdc1
Ankit Ojha <ankitojha@Ankits-MacBook-Pro.local>
2024-10-18 20:06:30 -0700

    Initial Commit

---
+++ translation_back/translation-service.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // Add this line to parse JSON body
+
+const googleApiKey = 'AIzaSyDXjtOASiUIr917oGx2o47CuaRLb0I6v38';
+
+app.post('/translate', async (req, res) => {
+    const { text, targetLanguage } = req.body;
+
+    try {
+        const response = await axios.post('https://translation.googleapis.com/language/translate/v2', null, {
+            params: {
+                q: text,
+                target: targetLanguage,
+                key: googleApiKey,
+            },
+        });
+
+        res.status(200).json({ translatedText: response.data.data.translations[0].translatedText });
+    } catch (error) {
+        console.error('Translation Error:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(5002, () => {
+    console.log('Translation Service running on port 5002');
+});
